b57a89e5732e9fbd00d04073fd12921f72288d21
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-04-25 09:02:23 +0200

    Seccion 4: add services.  HeroService and ObservableData

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +8,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((data) => (this.heroes = data));
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
